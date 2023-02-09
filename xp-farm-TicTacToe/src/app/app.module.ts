adb877d822c5b717b6318f466e691845a4f58ba1
GUILLERMO DE JESUS ANDRADE GONZALEZ <guillermo.andrade@ibm.com>
2023-02-09 14:09:13 -0600

    add material modules

--- xp-farm-TicTacToe/src/app/app.module.ts
+++ xp-farm-TicTacToe/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Material Imports */
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
